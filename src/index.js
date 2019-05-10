f749f92571fd4ef172ed869e0853078b70bdc384
zouyun <thunderrun2035@gmail.com>
2019-05-10 14:17:05 +0800

    update test instance

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-# simplify-tree-nodes
-
-Replace child nodes' keys with top level parent node's key by given tree.
-
-## Example
-
-```js
-import simplifyTreeNodes from 'simplify-tree-nodes'
+import simplifyTreeNodes from "../index";
 
 const treeData = [
   {
@@ -47,5 +40,4 @@ const childKeys = ["0-0-0", "0-1-0", "0-1-1", "0-1-2-0", "0-1-2-1"];
 
 const keys = simplifyTreeNodes(childKeys, treeData);
 
-// keys: ['0-0-0', '0-1']
-```
+console.log(keys);
